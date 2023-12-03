3f182380f458a0f7449d1876084514274c21d39f
gorapaduckoo <jioung93@naver.com>
2023-12-03 19:17:21 +0900

    chore: Spring Boot 초기 세팅

---
+++ src/main/java/weavers/siltarae/SiltaraeBeApplication.java
@@ -0,0 +1,13 @@
+package weavers.siltarae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiltaraeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiltaraeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/weavers/siltarae/SiltaraeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package weavers.siltarae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiltaraeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
