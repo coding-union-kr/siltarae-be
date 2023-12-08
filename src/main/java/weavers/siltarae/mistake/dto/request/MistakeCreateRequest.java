3b3a733ba2891a61ab7b4276d66b08f78bf3eccf
dowoon <woodw920@gmail.com>
2023-12-08 16:52:43 +0900

    feat: 실수 생성 Request 작성

---
+++ src/main/java/weavers/siltarae/mistake/dto/request/MistakeCreateRequest.java
@@ -0,0 +1,21 @@
+package weavers.siltarae.mistake.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MistakeCreateRequest {
+    private List<Long> tagIds;
+    @NotNull(message = "실수 내용은 필수 입니다.")
+    @Size(max = 140, message = "실수 내용은 140자를 넘을 수 없습니다.")
+    private String content;
+}
