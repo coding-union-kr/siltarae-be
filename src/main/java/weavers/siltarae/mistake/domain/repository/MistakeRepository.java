6c4fde625fa2d1d8f11779bcfae02105927d7bd3
dowoon <woodw920@gmail.com>
2023-12-08 23:23:34 +0900

    feat: 실수 리스트 조회 Pageable 이용한 페이징처리 Repository 작성

--- src/main/java/weavers/siltarae/mistake/domain/repository/MistakeRepository.java
+++ src/main/java/weavers/siltarae/mistake/domain/repository/MistakeRepository.java
@@ -1,7 +1,11 @@
 package weavers.siltarae.mistake.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import weavers.siltarae.mistake.domain.Mistake;
+import weavers.siltarae.user.domain.User;
 
 public interface MistakeRepository extends JpaRepository<Mistake, Long> {
+    Page<Mistake> findByUserOrderByIdDesc(User user, Pageable pageable);
 }
