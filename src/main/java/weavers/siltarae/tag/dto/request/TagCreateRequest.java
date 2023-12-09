28aa427398cec7249d131293d02abf683a9ebe4a
gorapaduckoo <jioung93@naver.com>
2023-12-09 18:49:44 +0900

    fix: 태그명에 언더바 연속 2개 이상 사용 불가하도록 수정

--- src/main/java/weavers/siltarae/tag/dto/request/TagCreateRequest.java
+++ src/main/java/weavers/siltarae/tag/dto/request/TagCreateRequest.java
@@ -1,7 +1,6 @@
 package weavers.siltarae.tag.dto.request;
 
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
@@ -16,7 +15,7 @@ public class TagCreateRequest {
 
     @NotEmpty(message = "태그명을 입력해주세요.")
     @Length(max = 10, message = "태그명은 최대 10자입니다.")
-    @Pattern(regexp = "^[0-9a-zA-Zㄱ-ㅎ가-힣_]*$", message = "태그명에는 한글, 영어, 숫자, _만 사용 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+(?:_[a-zA-Z0-9가-힣]+)*$", message = "태그명에는 한글, 영어, 숫자, 연속 1개 이하의 _만 사용 가능합니다.")
     private String name;
 
     @Builder
