134b0bc0b0ff2df91e6f2ad3604bc66b630b7112
gorapaduckoo <jioung93@naver.com>
2023-12-10 00:36:09 +0900

    chore: 태그 조회 요청 테스트에서 Mockito 대신 BDDMockito 사용

--- src/test/java/weavers/siltarae/tag/controller/TagControllerTest.java
+++ src/test/java/weavers/siltarae/tag/controller/TagControllerTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -84,8 +84,8 @@ class TagControllerTest {
     @Test
     void 태그_목록_조회_요청에_성공한다() throws Exception {
         // given
-        when(tagService.getTagList(any(Long.class)))
-                .thenReturn(TagListResponse.from(List.of(COMPANY_TAG(), DAILY_TAG())));
+        given(tagService.getTagList(any(Long.class)))
+                .willReturn(TagListResponse.from(List.of(COMPANY_TAG(), DAILY_TAG())));
 
         // when
         final ResultActions resultActions = mockMvc.perform(get("/api/v1/tags")
