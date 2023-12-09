d5eab1e5fbcdc5d89ed424d38cf761fa62a7f664
gorapaduckoo <jioung93@naver.com>
2023-12-09 22:14:39 +0900

    test: 사용자의 태그 목록 조회 테스트 구현

--- src/test/java/weavers/siltarae/tag/controller/TagControllerTest.java
+++ src/test/java/weavers/siltarae/tag/controller/TagControllerTest.java
@@ -7,12 +7,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import weavers.siltarae.tag.dto.request.TagCreateRequest;
+import weavers.siltarae.tag.dto.response.TagListResponse;
 import weavers.siltarae.tag.service.TagService;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static weavers.siltarae.tag.TagTestFixture.COMPANY_TAG;
+import static weavers.siltarae.tag.TagTestFixture.DAILY_TAG;
 
 @WebMvcTest(TagController.class)
 class TagControllerTest {
@@ -33,7 +43,7 @@ class TagControllerTest {
         
 
         // when
-        final ResultActions resultActions = mockMvc.perform(post("/api/v1/category")
+        final ResultActions resultActions = mockMvc.perform(post("/api/v1/tags")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(tagCreateRequest)));
 
@@ -47,7 +57,7 @@ class TagControllerTest {
         final TagCreateRequest tagCreateRequest = new TagCreateRequest("VeryLongTagName");
 
         // when
-        final ResultActions resultActions = mockMvc.perform(post("/api/v1/category")
+        final ResultActions resultActions = mockMvc.perform(post("/api/v1/tags")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(tagCreateRequest)));
 
@@ -62,12 +72,32 @@ class TagControllerTest {
         final TagCreateRequest tagCreateRequest = new TagCreateRequest("Tag$name");
 
         // when
-        final ResultActions resultActions = mockMvc.perform(post("/api/v1/category")
+        final ResultActions resultActions = mockMvc.perform(post("/api/v1/tags")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(tagCreateRequest)));
 
         // then
         resultActions.andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("태그명에는 한글, 영어, 숫자, _만 사용 가능합니다."));
+    }
+
+    @Test
+    void 태그_목록_조회_요청에_성공한다() throws Exception {
+        // given
+        when(tagService.getTagList(any(Long.class)))
+                .thenReturn(TagListResponse.from(List.of(COMPANY_TAG(), DAILY_TAG())));
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get("/api/v1/tags")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        final MvcResult mvcResult = resultActions.andExpect(status().isOk())
+                .andReturn();
+        final TagListResponse tagListResponse = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                TagListResponse.class
+        );
+        assertThat(tagListResponse.getTotalCount()).isEqualTo(2);
     }
 }
