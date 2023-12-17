5e18e00ca7a762eea9df44e3d3a2e43fe6ef2557
dowoon <woodw920@gmail.com>
2023-12-18 03:14:55 +0900

    feat: 좋아요(Likes) Controller & Service 작성 - 좋아요 Controller 작성 및 Service 작성

---
+++ src/main/java/weavers/siltarae/like/controller/LikeController.java
@@ -0,0 +1,28 @@
+package weavers.siltarae.like.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import weavers.siltarae.like.service.LikeService;
+
+@Tag(name = "[좋아요] 좋아요 Controller")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/like")
+public class LikeController {
+
+    private static final Long TEST_USER_ID = 1L;
+    private final LikeService likeService;
+    @GetMapping
+    public ResponseEntity<Void> addLike(
+            @RequestParam("mistakeId") Long mistakeId) {
+        likeService.like(TEST_USER_ID, mistakeId);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
