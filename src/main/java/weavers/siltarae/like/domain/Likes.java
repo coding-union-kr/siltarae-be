f5db4484267781df5da5d91ef2af8eb336befcb3
dowoon <woodw920@gmail.com>
2023-12-18 03:13:53 +0900

    feat: 좋아요(Likes) 도메인 작성 - 좋아요 도메인 CreatedAt 자동 생성을 위한 abstract class 작성

--- src/main/java/weavers/siltarae/like/domain/Likes.java
+++ src/main/java/weavers/siltarae/like/domain/Likes.java
@@ -2,19 +2,22 @@ package weavers.siltarae.like.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import weavers.siltarae.global.CreatedAt;
 import weavers.siltarae.member.domain.Member;
 import weavers.siltarae.mistake.domain.Mistake;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 
 @Entity
 @IdClass(LikeId.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Likes {
+public class Likes extends CreatedAt {
     @Id
     @ManyToOne(optional = false, cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
@@ -25,7 +28,9 @@ public class Likes {
     @JoinColumn(name = "mistake_id", nullable = false)
     private Mistake mistake;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    @Builder
+    public Likes(Member member, Mistake mistake) {
+        this.member = member;
+        this.mistake = mistake;
+    }
 }
