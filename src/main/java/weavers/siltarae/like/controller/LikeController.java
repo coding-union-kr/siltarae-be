433a7f56a552d5b20c8b37a29eceec9606385640
dowoon <woodw920@gmail.com>
2023-12-18 18:51:15 +0900

    feat: like Controller Post 요청으로 변경

--- src/main/java/weavers/siltarae/like/controller/LikeController.java
+++ src/main/java/weavers/siltarae/like/controller/LikeController.java
@@ -3,10 +3,7 @@ package weavers.siltarae.like.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import weavers.siltarae.like.service.LikeService;
 
 @Tag(name = "[좋아요] 좋아요 Controller")
@@ -17,7 +14,7 @@ public class LikeController {
 
     private static final Long TEST_USER_ID = 1L;
     private final LikeService likeService;
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Void> addLike(
             @RequestParam("mistakeId") Long mistakeId) {
         likeService.like(TEST_USER_ID, mistakeId);
