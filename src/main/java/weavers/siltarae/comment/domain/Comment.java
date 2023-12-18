e0cbbb9414113aabca1ee69b5a39c0ba09f32cb4
dowoon <woodw920@gmail.com>
2023-12-18 14:11:50 +0900

    feat: Comment 도메인, Repository 작성

---
+++ src/main/java/weavers/siltarae/comment/domain/Comment.java
@@ -0,0 +1,39 @@
+package weavers.siltarae.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import weavers.siltarae.global.BaseEntity;
+import weavers.siltarae.member.domain.Member;
+import weavers.siltarae.mistake.domain.Mistake;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, length = 140)
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "mistake_id")
+    private Mistake mistake;
+
+    @Builder
+    public Comment(Long id, Member member, String content, Mistake mistake) {
+        this.id = id;
+        this.member = member;
+        this.content = content;
+        this.mistake = mistake;
+    }
+}
