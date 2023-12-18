d32ef9f420c521fa9fdffb0810f704ddf37a72c2
dowoon <woodw920@gmail.com>
2023-12-18 20:58:41 +0900

    feat: Repository 듀얼키로 등록

--- src/main/java/weavers/siltarae/like/domain/repository/LikeRepository.java
+++ src/main/java/weavers/siltarae/like/domain/repository/LikeRepository.java
@@ -1,12 +1,13 @@
 package weavers.siltarae.like.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import weavers.siltarae.like.domain.LikeId;
 import weavers.siltarae.like.domain.Likes;
 import weavers.siltarae.member.domain.Member;
 import weavers.siltarae.mistake.domain.Mistake;
 
 import java.util.Optional;
 
-public interface LikeRepository extends JpaRepository<Likes, Likes> {
+public interface LikeRepository extends JpaRepository<Likes, LikeId> {
     Optional<Likes> findByMemberAndMistake(Member member, Mistake mistake);
 }
