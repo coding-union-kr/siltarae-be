931451077294769e81819037681a591d1a14ef13
gorapaduckoo <jioung93@naver.com>
2023-12-22 00:39:30 +0900

    fix: 태그 삭제 테스트 관련 버그 수정 - 태그 삭제 테스트 시, UnsupportedOperationException 발생 - TagFixture에 실수 리스트를 넣어줄 때, List.of() 대신 ArrayList.asList() 사용

--- src/test/java/weavers/siltarae/tag/TagTestFixture.java
+++ src/test/java/weavers/siltarae/tag/TagTestFixture.java
@@ -1,7 +1,10 @@
 package weavers.siltarae.tag;
 
+import weavers.siltarae.member.domain.Member;
+import weavers.siltarae.member.domain.SocialType;
 import weavers.siltarae.tag.domain.Tag;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TagTestFixture {
@@ -9,28 +12,40 @@ public class TagTestFixture {
     public static Tag COMPANY_TAG() {
         return Tag.builder()
                 .id(1L)
+                .member(new Member(1L, "12345", "실타래", "siltarae@nn.com", SocialType.GOOGLE))
                 .name("회사")
-                .mistakes(List.of())
+                .mistakes(Arrays.asList())
                 .build();
     }
 
     public static Tag DAILY_TAG() {
         return Tag.builder()
-                .id(1L)
+                .id(2L)
+                .member(new Member(1L, "12345", "실타래", "siltarae@nn.com", SocialType.GOOGLE))
                 .name("일상")
-                .mistakes(List.of())
+                .mistakes(Arrays.asList())
                 .build();
     }
 
     public static Tag DELETED_TAG() {
         Tag deletedTag = Tag.builder()
-                            .id(1L)
+                            .id(3L)
+                            .member(new Member(1L, "12345", "실타래", "siltarae@nn.com", SocialType.GOOGLE))
                             .name("모의고사")
-                            .mistakes(List.of())
+                            .mistakes(Arrays.asList())
                             .build();
 
         deletedTag.delete();
 
         return deletedTag;
+    }
+
+    public static Tag OTHERS_TAG() {
+        return Tag.builder()
+                .id(4L)
+                .member(new Member(2L, "54321", "꿀타래", "kkultarae@nn.com", SocialType.GOOGLE))
+                .name("학생회")
+                .mistakes(Arrays.asList())
+                .build();
     }
 }
