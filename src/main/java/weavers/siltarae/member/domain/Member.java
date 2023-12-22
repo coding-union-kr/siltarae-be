a6af1bc7631ac1d62898e5e946d8de8fb5c7e430
gorapaduckoo <jioung93@naver.com>
2023-12-22 17:37:47 +0900

    feat: 멤버 탈퇴 기능 추가

--- src/main/java/weavers/siltarae/member/domain/Member.java
+++ src/main/java/weavers/siltarae/member/domain/Member.java
@@ -13,22 +13,33 @@ import weavers.siltarae.global.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
+    private static final String DELETED_MEMBER_NICKNAME = "탈퇴회원";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column
     private String identifier;
 
     @Column(nullable = false, length = 10)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column
     private String email;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(nullable = false)
+    @Column
     private SocialType socialType;
+
+    @Override
+    public void delete() {
+        super.delete();
+
+        this.identifier = null;
+        this.nickname = DELETED_MEMBER_NICKNAME;
+        this.email = null;
+    }
 
     @Builder
     public Member(Long id, String identifier, String nickname, String email, SocialType socialType) {
