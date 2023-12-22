570877b862dc7e9b42a218463c83bf2dbde9c899
dowoon <woodw920@gmail.com>
2023-12-22 23:19:28 +0900

    feat: mistake 정보에 Tag 정보 포함하는 service 로직 작성

--- src/main/java/weavers/siltarae/mistake/domain/repository/MistakeRepository.java
+++ src/main/java/weavers/siltarae/mistake/domain/repository/MistakeRepository.java
@@ -3,16 +3,17 @@ package weavers.siltarae.mistake.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import weavers.siltarae.mistake.domain.Mistake;
 import weavers.siltarae.member.domain.Member;
+import weavers.siltarae.mistake.domain.Mistake;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MistakeRepository extends JpaRepository<Mistake, Long>, MistakeQRepository {
-    Page<Mistake> findByMemberAndDeletedAtIsNullOrderByIdDesc(Member member, Pageable pageable);
+    Page<Mistake> findByMember_IdAndDeletedAtIsNullOrderByIdDesc(Long memberId, Pageable pageable);
     Optional<Mistake> findByIdAndMemberAndDeletedAtIsNull(Long id, Member member);
     List<Mistake> findByIdInAndMemberAndDeletedAtIsNull(List<Long> id, Member member);
     Optional<Mistake> findByIdAndDeletedAtIsNull(Long id);
     Page<Mistake> findByDeletedAtIsNullOrderByIdDesc(Pageable pageable);
+    Page<Mistake> findByMember_IdAndTags_IdInAndDeletedAtIsNullOrderByIdDesc(Long memberId, List<Long> tagIds, Pageable pageable);
 }
