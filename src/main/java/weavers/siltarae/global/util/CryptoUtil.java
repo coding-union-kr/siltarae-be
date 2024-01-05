6aaf6765e4aa8b301e3d2f11a615776538c94b5a
dowoon <woodw920@gmail.com>
2024-01-05 12:00:44 +0900

    refactor: 복호화, 암호화 중복 코드 메소드 분리하여 사용

--- src/main/java/weavers/siltarae/global/util/CryptoUtil.java
+++ src/main/java/weavers/siltarae/global/util/CryptoUtil.java
@@ -6,9 +6,12 @@ import weavers.siltarae.global.exception.BadRequestException;
 import weavers.siltarae.global.exception.ExceptionCode;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Component
@@ -24,11 +27,7 @@ public class CryptoUtil {
 
     public static String encrypt(String value) {
         try {
-            byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
-
-            Key key = new SecretKeySpec(keyBytes, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
+            Cipher cipher = createCipherForMode(Cipher.ENCRYPT_MODE);
 
             byte[] encrypted = cipher.doFinal(value.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(encrypted);
@@ -39,15 +38,21 @@ public class CryptoUtil {
 
     public static String decrypt(String encrypted) {
         try {
-            byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
-            Key key = new SecretKeySpec(keyBytes, ALGORITHM);
-            Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
-            cipher.init(Cipher.DECRYPT_MODE, key);
+            Cipher cipher = createCipherForMode(Cipher.DECRYPT_MODE);
 
             byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
             return new String(original, StandardCharsets.UTF_8);
         } catch (Exception e) {
             throw new BadRequestException(ExceptionCode.FAIL_DECRYPT);
         }
+    }
+
+    private static Cipher createCipherForMode(int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+
+        Key key = new SecretKeySpec(keyBytes, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        cipher.init(encryptMode, key);
+        return cipher;
     }
 }
