e0684d91e7d1a43dd59620f2e7b66e323d936bda
dowoon <woodw920@gmail.com>
2024-01-05 14:51:11 +0900

    refactor: 암호화 CBC 방법으로 변경

--- src/main/java/weavers/siltarae/global/util/CryptoUtil.java
+++ src/main/java/weavers/siltarae/global/util/CryptoUtil.java
@@ -7,22 +7,27 @@ import weavers.siltarae.global.exception.ExceptionCode;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.util.Base64;
 
 @Component
 public class CryptoUtil {
     private static final String ALGORITHM = "AES";
-    private static final String CIPHER_TRANSFORMATION = "AES/ECB/PKCS5Padding";
+    private static final String CIPHER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
     private static String secretKey;
+    private static String iv;
 
     @Value("${privacy.encryption.secretKey}")
     public void setSecretKey(String secretKey) {
         CryptoUtil.secretKey = secretKey;
+    }
+
+    @Value("${privacy.encryption.iv}")
+    public void setIv(String iv) {
+        CryptoUtil.iv = iv;
     }
 
     public static String encrypt(String value) {
@@ -47,12 +52,15 @@ public class CryptoUtil {
         }
     }
 
-    private static Cipher createCipherForMode(int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    private static Cipher createCipherForMode(int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        byte[] ivByte = Base64.getDecoder().decode(iv);
 
         Key key = new SecretKeySpec(keyBytes, ALGORITHM);
         Cipher cipher = Cipher.getInstance(CIPHER_TRANSFORMATION);
-        cipher.init(encryptMode, key);
+        IvParameterSpec ivSpec = new IvParameterSpec(ivByte);
+
+        cipher.init(encryptMode, key, ivSpec);
         return cipher;
     }
 }
