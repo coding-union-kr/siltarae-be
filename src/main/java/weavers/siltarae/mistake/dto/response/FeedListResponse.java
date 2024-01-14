b9a8b85c6859973f64bc8814ffa90c3162207b65
dowoon <woodw920@gmail.com>
2024-01-14 16:13:51 +0900

    fix: 피드와 실수조회 시 삭제된 댓글은 카운트 되지 않게끔 수정

--- src/main/java/weavers/siltarae/mistake/dto/response/FeedListResponse.java
+++ src/main/java/weavers/siltarae/mistake/dto/response/FeedListResponse.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import weavers.siltarae.mistake.domain.Mistake;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static lombok.AccessLevel.PROTECTED;
 
