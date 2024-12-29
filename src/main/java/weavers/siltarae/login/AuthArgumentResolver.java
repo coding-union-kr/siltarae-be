83a66d7d46f35f4f7fd2b633587b7f9a045c33bc
gorapaduckoo <jioung93@naver.com>
2024-12-29 19:17:45 +0900

    refactor: AuthArgumentResolver에서 JWT 검증 로직 제거

--- src/main/java/weavers/siltarae/login/AuthArgumentResolver.java
+++ src/main/java/weavers/siltarae/login/AuthArgumentResolver.java
@@ -1,6 +1,5 @@
 package weavers.siltarae.login;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -8,16 +7,13 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import weavers.siltarae.login.infrastructure.TokenProvider;
-
-import static org.springframework.http.HttpHeaders.*;
+import weavers.siltarae.global.auth.AuthContext;
 
 @Component
 @RequiredArgsConstructor
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private final AccessTokenExtractor accessTokenExtractor;
-    private final TokenProvider tokenProvider;
+    private final AuthContext authContext;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -28,17 +24,7 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        Auth annotation = parameter.getParameterAnnotation(Auth.class);
-
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String authorizationHeader = request.getHeader(AUTHORIZATION);
-
-        if(!annotation.required() && authorizationHeader==null) {
-            return null;
-        }
-
-        String accessToken = accessTokenExtractor.extractAccessToken(authorizationHeader);
-        return tokenProvider.getMemberIdFromAccessToken(accessToken);
+    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        return authContext.getMemberId();
     }
 }
