d3e8d0f4764fd815641ad7c95bd5f4e7ab702196
gorapaduckoo <67768976+gorapaduckoo@users.noreply.github.com>
2024-12-30 08:12:10 +0900

    refactor: [#205] JWT 검증 방법 수정 (#206)
    * deploy: dockerFile 포트 변경
    
    * deploy: resources 폴더 진입 스크립트 추가
    
    * deploy: 백엔드 버그 수정 내역 배포 (#203)
    
    * fix: 로그아웃 시 리프레시 토큰 쿠키 삭제 로직 추가 (#198)
    
    * fix: 태그명 중복 검사 시, 삭제된 태그는 중복 검사 대상에서 제외 (#200)
    
    * fix: 댓글 등록 Response 에 memberImageUrl 추가
    
    ---------
    
    Co-authored-by: dowoon <woodw920@gmail.com>
    
    * docs: 서비스 아키텍처 및 위키 링크 추가
    
    - 서비스 아키텍처 이미지를 추가했어요.
    - 실타래 위키 링크를 추가했어요.
    
    * feat: AuthContext 추가
    
    * feat: 회원 전용 API용 인터셉터 추가
    
    * feat: 공용 API용 인터셉터 추가
    
    * refactor: AuthArgumentResolver에서 JWT 검증 로직 제거
    
    * refactor: @Auth 어노테이션에서 required 속성 제거
    
    * config: Auth 관련 인터셉터 설정 추가
    
    ---------
    
    Co-authored-by: Dowoon <woodw920@gmail.com>

---
+++ src/main/java/weavers/siltarae/global/auth/OptionalAuthInterceptor.java
@@ -0,0 +1,36 @@
+package weavers.siltarae.global.auth;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import weavers.siltarae.login.AccessTokenExtractor;
+import weavers.siltarae.login.infrastructure.TokenProvider;
+
+@Component
+@RequiredArgsConstructor
+public class OptionalAuthInterceptor implements HandlerInterceptor {
+
+    private final AccessTokenExtractor accessTokenExtractor;
+    private final AuthContext authContext;
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader == null) {
+            return true;
+        }
+
+        String accessToken = accessTokenExtractor.extractAccessToken(authorizationHeader);
+        Long memberId = tokenProvider.getMemberIdFromAccessToken(accessToken);
+        authContext.setMemberId(memberId);
+
+        return true;
+    }
+}
